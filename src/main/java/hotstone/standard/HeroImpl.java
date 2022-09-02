21afbf1b319f16f48e81e09045cdba50d8c727c8
au649616 <au649616@cs.au.dk>
2022-09-02 16:13:09 +0200

    trying new implementation with Hand Deck and Field on Hero

--- src/main/java/hotstone/standard/HeroImpl.java
+++ src/main/java/hotstone/standard/HeroImpl.java
@@ -1,7 +1,10 @@
 package hotstone.standard;
 
+import hotstone.framework.Card;
 import hotstone.framework.Hero;
 import hotstone.framework.Player;
+
+import java.util.ArrayList;
 
 public class HeroImpl implements Hero {
     private int mana;
@@ -10,12 +13,19 @@ public class HeroImpl implements Hero {
     private Player owner;
     private String heroPower;
 
+    private ArrayList<Card> field;
+    private ArrayList<Card> hand;
+    private ArrayList<Card> deck;
+
     public HeroImpl(String type,Player owner){
         this.health=GameConstants.HERO_MAX_HEALTH;
         this.type=type;
         this.owner=owner;
         mana=3;
         this.heroPower="Cute";
+        field = new ArrayList<>();
+        hand = new ArrayList<>();
+        deck= new ArrayList<>();
     }
     @Override
     public int getMana() {
@@ -47,4 +57,20 @@ public class HeroImpl implements Hero {
     public String getHeroPower(){
         return heroPower;
     }
+    public ArrayList<Card> getField(){
+        return field;
+    }
+    public ArrayList<Card> getHand(){
+        return hand;
+    }
+    public void updateHand(ArrayList<Card> hand){
+        this.hand=hand;
+    }
+    public ArrayList<Card> getDeck(){
+        return deck;
+    }
+    public void updateDeck(ArrayList<Card> deck){
+        this.deck=deck;
+    }
+
 }
